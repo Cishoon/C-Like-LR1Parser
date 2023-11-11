47b9c38b5f89387e4d90152c1ba549cf33f3aaff
Cishoon <1329115097@qq.com>
2023-11-11 22:34:25 +0800

    feat: 语法分析器

---
+++ src/LR1Parser.cpp
@@ -0,0 +1,428 @@
+#include <iostream>
+#include "LR1Parser.hpp"
+
+LR1Parser::LR1Parser(const std::vector<Production>& productions, Symbol start, Symbol end)
+    : productions(productions), start_symbol(start), end_symbol(end)
+{
+	for (const auto& production : productions) {
+		productionMap[production.lhs].push_back(production);
+	}
+	calculateFirstSets();
+	construct_tables();
+}
+
+LR1Parser::LR1Parser(const std::string file_path)
+{
+	std::ifstream file(file_path);
+	if (!file.is_open()) {
+		std::cerr << "无法打开文件: " + file_path << '\n';
+	}
+
+	std::string first_line;
+	if (!std::getline(file, first_line)) {
+		std::cerr << "文件格式错误: 第首行必须定义起始符合终止符\n";
+	}
+	std::istringstream is(first_line);
+	std::string temp;
+	is >> temp;
+	start_symbol = Symbol(SymbolType::NonTerminal, temp);
+	is >> temp;
+	end_symbol = Symbol(SymbolType::Terminal, temp);
+
+	std::string terminalsLine;
+	if (!std::getline(file, terminalsLine)) {
+		std::cerr << "文件格式错误: 第二行必须定义终结符\n";
+	}
+	std::istringstream iss(terminalsLine);
+	std::string terminal;
+	while (iss >> terminal) {
+		terminals.insert(terminal);
+	}
+
+	std::string line;
+	while (std::getline(file, line)) {
+		parseEBNFLine(line);
+	}
+
+	calculateFirstSets();
+	std::cout << 1;
+	construct_tables();
+}
+
+void LR1Parser::parseEBNFLine(const std::string& line)
+{
+	std::istringstream iss(line);
+	std::string lhs, rhsPart, token;
+
+	// 读取非终结符，自动忽略前导空格
+	iss >> lhs;
+	// 忽略 "::="
+	std::string ignoreStr;
+	std::getline(iss, ignoreStr, '=');
+
+	// 读取并解析产生式右侧
+	std::getline(iss, rhsPart);
+	std::istringstream rhsStream(rhsPart);
+
+	// 用 '|' 分割产生式右边的语句
+	while (std::getline(rhsStream, token, '|')) {
+		std::istringstream tokenStream(token);
+		std::vector<Symbol> rhsSymbols;
+
+		// 获取产生式右边的符号
+		std::string sym;
+		while (tokenStream >> sym) {
+			SymbolType type;
+			if (sym == "Epsilon") {
+				type = SymbolType::Epsilon;
+			} else {
+				type = terminals.find(sym) == terminals.end() ? SymbolType::NonTerminal : SymbolType::Terminal;
+			}
+			rhsSymbols.push_back(Symbol(type, sym));
+		}
+
+		// 创建产生式并添加到某个容器中
+		productionMap[Symbol(SymbolType::NonTerminal, lhs)].push_back(Production{Symbol(SymbolType::NonTerminal, lhs), rhsSymbols});
+		productions.push_back(Production(Symbol(SymbolType::NonTerminal, lhs), rhsSymbols));
+	}
+}
+
+
+void LR1Parser::calculateFirstSets()
+{
+	for (const auto& [nonTerminal, _] : productionMap) {
+		firstSet[nonTerminal] = first(nonTerminal);
+	}
+}
+
+std::set<Symbol> LR1Parser::first(const Symbol& symbol) const
+{
+	if (symbol.type == SymbolType::Terminal || symbol.type == SymbolType::Epsilon) {  // 终结符或空串
+		return {symbol};
+	}
+
+	std::set<Symbol> result;
+	for (const auto& prod : productionMap.at(symbol)) {
+		std::set<Symbol> prodFirstSet = firstString(prod.rhs);
+		result.insert(prodFirstSet.begin(), prodFirstSet.end());
+	}
+
+	return result;
+}
+
+std::set<Symbol> LR1Parser::firstString(const std::vector<Symbol>& str) const
+{
+	std::set<Symbol> result;
+	for (const Symbol& symbol : str) {
+		std::set<Symbol> symbolFirstSet = first(symbol);
+		bool containsEpsilon = symbolFirstSet.erase(Symbol(SymbolType::Epsilon, "")) > 0;
+		result.insert(symbolFirstSet.begin(), symbolFirstSet.end());
+		if (!containsEpsilon) {
+			return result;
+		}
+	}
+	result.insert(Symbol(SymbolType::Epsilon, ""));
+	return result;
+}
+
+LR1Item::State LR1Parser::get_lr1item_state(const LR1Item& item) const
+{
+	const auto& production = item.production;
+	const auto& dot_position = item.dot_position;
+	const auto& lookahead = item.lookahead;
+
+	// 检查是否是接受状态
+	if (production.lhs == start_symbol && dot_position == production.rhs.size() && lookahead == end_symbol) {
+		return LR1Item::State::ACCEPT;
+	}
+
+	// 检查是否是归约状态
+	if (dot_position == production.rhs.size()) {
+		return LR1Item::State::REDUCE;
+	}
+
+	// 检查是否是移进状态
+	if (dot_position < production.rhs.size() && production.rhs[dot_position].type == SymbolType::Terminal) {
+		return LR1Item::State::SHIFT;
+	}
+
+	// 检查是否是转换状态
+	if (dot_position < production.rhs.size() && production.rhs[dot_position].type == SymbolType::NonTerminal) {
+		return LR1Item::State::GOTO;
+	}
+
+	// 如果没有匹配的状态，可能是一个错误或未定义的状态
+	throw std::runtime_error("Undefined state for LR1 item.");
+}
+
+std::set<LR1Item> LR1Parser::closure(std::set<LR1Item> old_set) const
+{
+	while (true) {
+		std::set<LR1Item> new_set(old_set);
+		for (auto it = old_set.begin(); it != old_set.end(); it++) {
+			auto& item = *it;
+
+			// 只有GOTO状态(.后面是非终结符)才需要处理
+			if (get_lr1item_state(item) != LR1Item::State::GOTO) continue;
+
+			Symbol next_symbol = item.next_symbol();
+			Symbol nnext_symbol = item.nnext_symbol();
+			// 找到文法中所有以next_symbol为lhs的产生式
+			std::vector<Production> next_prods = get_productions_start_by_symbol(next_symbol);
+			for (auto& prod : next_prods) {
+				std::set<Symbol> lookheads = firstString({nnext_symbol, item.lookahead});
+				for (auto p_lh = lookheads.begin(); p_lh != lookheads.end(); p_lh++) {
+					new_set.insert(LR1Item(prod, 0, *p_lh));
+				}
+			}
+		}
+		if (old_set == new_set) break;
+		old_set = new_set;
+	}
+	return old_set;
+}
+
+std::vector<Production> LR1Parser::get_productions_start_by_symbol(const Symbol& symbol) const
+{
+	auto it = productionMap.find(symbol);
+	if (it == productionMap.end()) return std::vector<Production>();
+	return it->second;
+}
+
+void LR1Parser::print_firstSet() const
+{
+	for (const auto& [symbol, firstSetSymbols] : firstSet) {
+		std::cout << "FIRST(" << symbol.value << ") = { ";
+		for (const auto& sym : firstSetSymbols) {
+			std::cout << sym.value << " ";
+		}
+		std::cout << "}\n";
+	}
+}
+
+void LR1Parser::print_tables() const
+{
+	// 打印ACTION表
+	std::cout << "ACTION Table:\n";
+	for (const auto& entry : actionTable) {
+		const auto& state_symbol_pair = entry.first;
+		const auto& action = entry.second;
+
+		std::cout << "State " << state_symbol_pair.first << ", Symbol " << state_symbol_pair.second.to_string()
+		          << ": " << action.to_string() << "\n";
+	}
+
+	// 打印GOTO表
+	std::cout << "\nGOTO Table:\n";
+	for (const auto& entry : gotoTable) {
+		const auto& state_symbol_pair = entry.first;
+		const auto& nextState = entry.second;
+
+		std::cout << "State " << state_symbol_pair.first << ", Symbol " << state_symbol_pair.second.to_string()
+		          << ": " << nextState << "\n";
+	}
+}
+
+void LR1Parser::construct_tables()
+{
+	Production begin_production = get_productions_start_by_symbol(start_symbol).at(0);
+	lr1ItemSets.push_back(std::set<LR1Item>({LR1Item(begin_production, 0, end_symbol)}));
+	lr1ItemSets[0] = closure(lr1ItemSets[0]);
+
+	for (size_t index = 0; index < lr1ItemSets.size(); ++index) {
+		std::vector<LR1Item> shift_items, reduce_items, accept_items, goto_items;
+		std::vector<Symbol> VT_shift, VN_goto;
+		// std::cout << index << '\n';
+		for (auto& item : lr1ItemSets[index]) {
+			// std::cout << item.to_string() << ' ' << (int)get_lr1item_state(item) << '\n';
+			switch (get_lr1item_state(item)) {
+				case LR1Item::State::ACCEPT:
+					accept_items.push_back(item);
+					break;
+				case LR1Item::State::GOTO:
+					goto_items.push_back(item);
+					VN_goto.push_back(item.next_symbol());
+					break;
+				case LR1Item::State::REDUCE:
+					reduce_items.push_back(item);
+					break;
+				case LR1Item::State::SHIFT:
+					shift_items.push_back(item);
+					VT_shift.push_back(item.next_symbol());
+					break;
+				default:
+					std::cerr << "construct_tables() 中LR1Item类型错误" << std::endl;
+			}
+		}
+
+		for (auto& vt : VT_shift) {
+			std::set<LR1Item> new_set;
+			for (auto& item : lr1ItemSets[index]) {
+				if (get_lr1item_state(item) == LR1Item::State::SHIFT && item.next_symbol() == vt) {
+					new_set.insert(LR1Item(item.production, item.dot_position + 1, item.lookahead));
+				}
+			}
+			new_set = closure(new_set);
+			size_t id = find(lr1ItemSets.begin(), lr1ItemSets.end(), new_set) - lr1ItemSets.begin();
+			if (id == lr1ItemSets.size()) {
+				lr1ItemSets.push_back(new_set);
+			}
+			actionTable[{index, vt}] = Action(Action::Type::SHIFT, id, Production());
+		}
+
+		for (auto& vn : VN_goto) {
+			std::set<LR1Item> new_set;
+			for (auto& item : lr1ItemSets[index]) {
+				if (get_lr1item_state(item) == LR1Item::State::GOTO && item.next_symbol() == vn) {
+					new_set.insert(LR1Item(item.production, item.dot_position + 1, item.lookahead));
+				}
+			}
+			new_set = closure(new_set);
+			size_t id = find(lr1ItemSets.begin(), lr1ItemSets.end(), new_set) - lr1ItemSets.begin();
+			if (id == lr1ItemSets.size()) {
+				lr1ItemSets.push_back(new_set);
+			}
+			gotoTable[{index, vn}] = id;
+		}
+
+		for (auto& item : accept_items) {
+			actionTable[{index, item.lookahead}] = Action(Action::Type::ACCEPT, -1, Production());
+		}
+
+		for (auto& item : reduce_items) {
+			actionTable[{index, item.lookahead}] = Action(Action::Type::REDUCE, -1, item.production);
+		}
+	}
+}
+
+bool LR1Parser::parse(const std::vector<Symbol>& sentence) const
+{
+	std::stack<int> stateStack;                                          // 状态栈
+	std::stack<Symbol> symbolStack;                                      // 符号栈
+	std::vector<Symbol> inputStack(sentence.rbegin(), sentence.rend());  // 输入栈
+
+	// 初始状态
+	stateStack.push(0);
+
+	while (!inputStack.empty()) {
+		int currentState = stateStack.top();
+		Symbol currentSymbol = inputStack.back();
+
+		// 打印当前栈的状态
+		print_stacks(stateStack, symbolStack, inputStack);
+
+		auto actionIt = actionTable.find({currentState, currentSymbol});
+		if (actionIt != actionTable.end()) {
+			const Action& action = actionIt->second;
+
+			switch (action.type) {
+				case Action::Type::SHIFT: {
+					stateStack.push(action.number);
+					symbolStack.push(currentSymbol);
+					inputStack.pop_back();
+					break;
+				}
+				case Action::Type::REDUCE: {
+					// 根据产生式右侧的长度，从栈中弹出相应数量的符号和状态
+					for (size_t i = 0; i < action.production.rhs.size(); ++i) {
+						symbolStack.pop();
+						stateStack.pop();
+					}
+					// 将产生式左侧的非终结符压入符号栈
+					symbolStack.push(action.production.lhs);
+
+					// 更新状态栈
+					int nextState = gotoTable.at({stateStack.top(), action.production.lhs});
+					stateStack.push(nextState);
+					break;
+				}
+				case Action::Type::ACCEPT:
+					std::cout << "Accept\n";
+					return true;
+				default:
+					std::cerr << "Parse error\n";
+					return false;
+			}
+		} else {
+			std::cerr << "Parse error: no action\n";
+			return false;
+		}
+	}
+
+	std::cerr << "Parse error: input not consumed\n";
+	return false;
+}
+
+void LR1Parser::print_stacks(const std::stack<int>& stateStack,
+                             const std::stack<Symbol>& symbolStack,
+                             const std::vector<Symbol>& inputStack) const
+{
+	// 使用向量来反转栈的内容
+	std::vector<int> reversedStateStack;
+	std::vector<Symbol> reversedSymbolStack;
+
+	// 从栈中复制到向量
+	auto tempStateStack = stateStack;
+	while (!tempStateStack.empty()) {
+		reversedStateStack.push_back(tempStateStack.top());
+		tempStateStack.pop();
+	}
+
+	auto tempSymbolStack = symbolStack;
+	while (!tempSymbolStack.empty()) {
+		reversedSymbolStack.push_back(tempSymbolStack.top());
+		tempSymbolStack.pop();
+	}
+
+	// 逆序打印向量，从而按栈底到栈顶的顺序输出
+	std::cout << "State Stack: ";
+	for (auto it = reversedStateStack.rbegin(); it != reversedStateStack.rend(); ++it) {
+		std::cout << *it << " ";
+	}
+	std::cout << "\n";
+
+	std::cout << "Symbol Stack: ";
+	for (auto it = reversedSymbolStack.rbegin(); it != reversedSymbolStack.rend(); ++it) {
+		std::cout << it->to_string() << " ";
+	}
+	std::cout << "\n";
+
+	std::cout << "Input Stack: ";
+	for (auto it = inputStack.rbegin(); it != inputStack.rend(); ++it) {
+		std::cout << it->to_string() << " ";
+	}
+	std::cout << "\n\n";
+}
+
+
+int main()
+{
+	// 定义非终结符
+	Symbol S(SymbolType::NonTerminal, "S");
+	Symbol B(SymbolType::NonTerminal, "B");
+	Symbol S_prime(SymbolType::NonTerminal, "S'");
+
+	// 定义终结符
+	Symbol a(SymbolType::Terminal, "a");
+	Symbol b(SymbolType::Terminal, "b");
+
+	// 定义产生式
+	Production p1(S_prime, {S});
+	Production p2(S, {B, B});
+	Production p3(B, {a, B});
+	Production p4(B, {b});
+
+	// std::vector<Production> productions = {p1, p2, p3, p4};
+
+	// // 创建LR1解析器实例
+	// LR1Parser parser(productions, S_prime, Symbol(SymbolType::Terminal, "$"));
+	LR1Parser parser("grammer.txt");
+
+	parser.print_firstSet();
+	parser.print_tables();
+
+	// std::vector<Symbol> sentence = {a, b, a, b, Symbol(SymbolType::Terminal, "$")};
+	// parser.parse(sentence);
+
+	return 0;
+}
---
+++ src/LR1Parser.hpp
@@ -0,0 +1,251 @@
+#include <string>
+#include <set>
+#include <vector>
+#include <map>
+#include <stack>
+#include <algorithm>
+#include <fstream>
+#include <sstream>
+
+
+enum class SymbolType {
+	Terminal,     // 终结符
+	NonTerminal,  // 非终结符
+	Epsilon       // 空串
+};
+
+struct Symbol
+{
+	SymbolType type;
+	std::string value;
+	Symbol()
+	{
+		type = SymbolType::Epsilon;
+		value = "";
+	}
+	Symbol(const SymbolType& type, const std::string& value) : type(type), value(value) {}
+
+	std::string to_string() const
+	{
+		return value;  // 或者任何合适的表示方式
+	}
+
+	friend bool operator==(const Symbol& lhs, const Symbol& rhs)
+	{
+		return lhs.type == rhs.type && lhs.value == rhs.value;
+	}
+
+	friend bool operator<(const Symbol& lhs, const Symbol& rhs)
+	{
+		if (lhs.type < rhs.type) return true;
+		if (rhs.type < lhs.type) return false;
+		return lhs.value < rhs.value;
+	}
+};
+
+struct Production
+{
+	Symbol lhs;               // 左手边，非终结符
+	std::vector<Symbol> rhs;  // 右手边，非终结符和终结符的 组合
+
+	Production() {}
+	Production(Symbol lhs, std::vector<Symbol> rhs) : lhs(lhs), rhs(rhs) {}
+	std::string to_string() const
+	{
+		std::string res;
+		res += lhs.value + " -> ";
+		for (auto& item : rhs) {
+			res += item.value;
+		}
+		return "[" + res + "]";
+	}
+
+	friend bool operator==(const Production& lhs, const Production& rhs)
+	{
+		return lhs.lhs == rhs.lhs && lhs.rhs == rhs.rhs;
+	}
+	friend bool operator<(const Production& lhs, const Production& rhs)
+	{
+		if (lhs.lhs < rhs.lhs) return true;
+		if (rhs.lhs < lhs.lhs) return false;
+		return lhs.rhs < rhs.rhs;
+	}
+};
+
+class LR1Item {
+public:
+	enum class State {
+		SHIFT,   // 移进
+		REDUCE,  // 归约
+		GOTO,    // 待约
+		ACCEPT   // 接受
+	};
+
+	LR1Item(Production production, size_t dot_position, Symbol lookahead)
+	    : production(production), dot_position(dot_position), lookahead(lookahead) {}
+
+	std::string to_string() const
+	{
+		std::string res;
+		res += production.lhs.value + " -> ";
+		size_t index = 0;
+		for (auto& item : production.rhs) {
+			if (index++ == dot_position) res += ".";
+			res += item.value;
+		}
+		if (index++ == dot_position) res += ".";
+		res += ", " + lookahead.value;
+		return "[" + res + "]";
+	}
+
+	Symbol next_symbol() const
+	{
+		if (dot_position == production.rhs.size()) return Symbol(SymbolType::Epsilon, "");
+		return production.rhs[dot_position];
+	}
+
+	Symbol nnext_symbol() const
+	{
+		if (dot_position == production.rhs.size()) return Symbol(SymbolType::Epsilon, "");
+
+		LR1Item tmp = *this;
+		tmp.dot_position++;
+		return tmp.next_symbol();
+	}
+
+	Production production;
+	size_t dot_position;
+	Symbol lookahead;
+
+	friend bool operator==(const LR1Item& lhs, const LR1Item& rhs)
+	{
+		return lhs.production == rhs.production &&
+		       lhs.dot_position == rhs.dot_position &&
+		       lhs.lookahead == rhs.lookahead;
+	}
+
+	friend bool operator<(const LR1Item& lhs, const LR1Item& rhs)
+	{
+		if (lhs.production < rhs.production) return true;
+		if (rhs.production < lhs.production) return false;
+		if (lhs.dot_position < rhs.dot_position) return true;
+		if (rhs.dot_position < lhs.dot_position) return false;
+		return lhs.lookahead < rhs.lookahead;
+	}
+};
+
+struct Action
+{
+	enum class Type {
+		SHIFT,
+		REDUCE,
+		ACCEPT,
+		ERROR
+	};
+	Type type;
+	size_t number;          // shift的新状态编号
+	Production production;  // reduce使用的产生式
+
+	Action() {}
+	Action(const Type& type, const size_t& number, const Production& production) : type(type), number(number), production(production) {}
+
+	std::string to_string() const
+	{
+		switch (type) {
+			case Type::SHIFT:
+				return "S " + std::to_string(number);
+			case Type::REDUCE:
+				return "R " + production.to_string();
+			case Type::ACCEPT:
+				return "Accept";
+			case Type::ERROR:
+				return "x";
+		}
+		return "";
+	}
+};
+
+
+class LR1Parser {
+public:
+	LR1Parser(const std::vector<Production>& productions, Symbol start, Symbol end);
+	LR1Parser(const std::string file_path);
+
+	void print_firstSet() const;
+	void print_tables() const;
+	bool parse(const std::vector<Symbol>& sentence) const;
+
+private:
+	void parseEBNFLine(const std::string& line);
+	void print_stacks(const std::stack<int>& stateStack,
+	                  const std::stack<Symbol>& symbolStack,
+	                  const std::vector<Symbol>& inputStack) const;
+
+	void construct_tables();
+	/**
+	 * @brief 求项目集族的闭包
+	 *
+	 * @param lr1ItemSet 待求闭包的项目集族
+	 * @return std::set<LR1Item> 该项目集族的闭包
+	 */
+	std::set<LR1Item> closure(std::set<LR1Item> lr1ItemSet) const;
+
+private:
+	/**
+	 * @brief 获取一个LR1项目当前的状态(移进、归约、接受、待约)
+	 *
+	 * @param item LR1项目
+	 * @return LR1Item::State 状态
+	 */
+	LR1Item::State get_lr1item_state(const LR1Item& item) const;
+
+	/**
+	 * @brief 获取由某非终结符为产生式左边的所有产生式
+	 *
+	 * @param symbol 非终结符
+	 * @return std::vector<Production> 所有产生式
+	 */
+	std::vector<Production> get_productions_start_by_symbol(const Symbol& symbol) const;
+
+private:  // 求FIRST集
+	void calculateFirstSets();
+	/**
+	 * @brief 计算并返回一个给定符号的FIRST集。
+	 *
+	 * FIRST集是指从一个符号（终结符或非终结符）开始的所有字符串的首个符号的集合。
+	 * - 如果该符号是一个终结符或空串（ε），FIRST集就是该符号本身。
+	 * - 如果该符号是一个非终结符，FIRST集包括所有以该非终结符开头的产生式右侧的字符串的首个符号。
+	 *   特别地，如果某个产生式的右侧可以推导出空串，空串也会包含在FIRST集中。
+	 *
+	 * @param symbol 要计算FIRST集的符号。
+	 * @return std::set<Symbol> 给定符号的FIRST集。
+	 */
+	std::set<Symbol> first(const Symbol& symbol) const;
+	/**
+	 * @brief 计算并返回一个符号串的FIRST集。
+	 *
+	 * 对于一个给定的符号串，其FIRST集是由从左到右遍历符号串时遇到的首个非空串符号的FIRST集组成。
+	 * - 如果符号串的某个符号的FIRST集包含空串（ε），则继续考虑该符号之后的符号。
+	 * - 如果遍历到串尾且所有符号的FIRST集都包含空串，那么空串也包含在整个串的FIRST集中。
+	 * - 如果符号串为空，那么其FIRST集也只包含空串。
+	 *
+	 * @param str 要计算FIRST集的符号串。
+	 * @return std::set<Symbol> 符号串的FIRST集。
+	 */
+	std::set<Symbol> firstString(const std::vector<Symbol>& str) const;
+
+private:
+	std::vector<Production> productions;
+	std::map<Symbol, std::vector<Production>> productionMap;
+	std::map<Symbol, std::set<Symbol>> firstSet, followSet;
+
+	Symbol start_symbol;  // 起始符
+	Symbol end_symbol;    // 终止符
+
+
+	std::map<std::pair<int, Symbol>, Action> actionTable;  // ACTION表
+	std::map<std::pair<int, Symbol>, int> gotoTable;       // GOTO表
+	std::vector<std::set<LR1Item>> lr1ItemSets;            // 项目集族
+
+	std::set<std::string> terminals;  // 终结符集
+};
