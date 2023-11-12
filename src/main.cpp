8e330acf166e69d26a13ccbd68321a524fa89f5a
Cishoon <1329115097@qq.com>
2023-11-12 11:49:01 +0800

    改成帯参主函数

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,21 @@
 #include "LR1Parser.hpp"
 #include <iostream>
 #include <fstream>
+#include <sstream>
 
-int main()
+int main(int argc, char* argv[])
 {
-	std::ifstream file("input.txt");
+	if (argc < 3) {
+		std::cerr << "用法: " << argv[0] << " <输入文件> <文法文件>" << std::endl;
+		return 1;
+	}
+
+	std::string inputFile = argv[1];
+	std::string grammarFile = argv[2];
+
+	std::ifstream file(inputFile);
 	if (!file.is_open()) {
-		std::cerr << "无法打开文件" << std::endl;
+		std::cerr << "无法打开文件: " << inputFile << std::endl;
 		return 1;
 	}
 
@@ -21,13 +30,12 @@ int main()
 	Token token;
 	do {
 		token = lexer.getNextToken();
-		std::cout << token.toString() << std::endl;
+		// std::cout << token.toString() << std::endl;
 		sentence.push_back(Symbol(SymbolType::Terminal, token.toString()));
 	} while (token.type != T_EOF);
 
-	LR1Parser parser("grammer.txt");
+	LR1Parser parser(grammarFile);
 	parser.parse(sentence);
-
 
 	return 0;
 }
